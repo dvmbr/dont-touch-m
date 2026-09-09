cf06aea849f5a18149212ff7f3a28ca034412296
dvmbr <itsdvmbr@gmail.com>
2026-09-09 23:18:07 +0900

    chore: initialize Spring Boot project

---
+++ backend/src/main/java/com/dvmbr/dtmb/DtmbApplication.java
@@ -0,0 +1,13 @@
+package com.dvmbr.dtmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtmbApplication.class, args);
+	}
+
+}
